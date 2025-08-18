4594a11a310bd72d21b600c8ff5294efd26ebcb8
Mazen <mazen.elgharib@ncsc.gov.bh>
2025-08-18 15:22:53 +0300

    Completed Part 1 Exercises

---
+++ part1/course-information/src/Content.jsx
@@ -0,0 +1,12 @@
+import Part from './part.jsx'
+const Content = ({course})=>{
+    return(
+        <div>
+            <Part part = {course.parts[0]}/>
+            <Part part = {course.parts[1]}/>
+            <Part part = {course.parts[2]}/>
+       </div>
+    )
+}
+
+export default Content 
